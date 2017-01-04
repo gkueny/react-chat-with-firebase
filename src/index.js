f7f177f49961c6a5074041df091ca15081de9b51
gkueny <gaetankueny@gmail.com>
2017-01-04 21:21:43 +0100

    init implementation of redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import allReducers from './reducers';
+
 import App from './components/App';
 import './assets/css/index.css';
 
+const store = createStore(allReducers);
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
