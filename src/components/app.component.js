2a29dcffa7dbed3b8cae67ea2d69a4ec32720868
gkueny <gaetankueny@gmail.com>
2017-01-04 21:54:02 +0100

    fix typo + fix random size bug

--- src/components/app.component.js
+++ src/components/app.component.js
@@ -13,13 +13,13 @@ class AppComponent extends Component {
     generateRandomText = () => {
 
         let text = "";
-        let maxSize = 25;
+        const maxSize = 15;
 
         const possible = "abcdefghijklmnopqrstuvwxyz ";
 
-        let size = Math.floor(Math.random() * maxSize);
+        const size = Math.floor(Math.random() * maxSize);
 
-        for( let i=0; i < maxSize; i++ )
+        for( let i=0; i < size; i++ )
             text += possible.charAt(Math.floor(Math.random() * possible.length));
 
         this.props.updateFakeMessage(text);
@@ -36,12 +36,12 @@ class AppComponent extends Component {
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Hey !</h2>
+                    <h2>Hey ! Bienvenue sur ton application React</h2>
                 </div>
                 <p className="App-intro">
-                    Poiur commencer, mettez à jour le fichier
+                    Pouur commencer, met à jour le fichier
                     <code> src/components/app.component.js </code>
-                    puis sauvez et actualisez.
+                    puis sauve et actualise.
                 </p>
                 <p className="App-intro">
                     Voici une suite de lettre random : <b>{this.props.state.general.fakeMessage}</b>
@@ -50,7 +50,7 @@ class AppComponent extends Component {
                     (Fais nous une capture d'écran si la suite signifie quelque-chose ! :D)
                 </p>
 
-                <button onClick={this.handleCLick}>Génerer un mot aléatoire !</button>
+                <button onClick={this.handleCLick}>Génerer une suite aléatoire !</button>
             </div>
         );
     }
